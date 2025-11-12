29c2f03e7c1551e6277ea9ead4653838db5ddbb0
yonatanfarhi20 <yonatanfarhi1@gmail.com>
2025-11-12 14:19:03 +0200

    with controller

---
+++ src/main/java/com/example/project_1/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.project_1.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> hello(){
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+
+}
